eaea5c81c53ae12b7f6f679b0cfa68864eaa0951
ktw1222 <ktw1222@gmail.com>
2017-07-06 18:27:19 -0400

    prepare starter files

--- src/App.js
+++ src/App.js
@@ -2,33 +2,14 @@ import React, { Component } from 'react';
 
 class ContactList extends React.Component {
   render() {
-    const people = this.props.contacts
 
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
 }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: "Michael" },
-          { name: "Ryan" },
-          { name: "Tyler" }
-        ]} />
+      <div>Hello World</div>
 
-        <ContactList contacts={[
-            { name: "Amanda" },
-            { name: "Richard" },
-            { name: "Justin" }
-          ]
-        }/>
-      </div>
     );
   }
 }
