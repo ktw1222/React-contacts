362986ccb47438f9aa56fdc83e8c149737301c8f
ktw1222 <ktw1222@gmail.com>
2017-07-06 18:46:28 -0400

    add backend server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 
-class ContactList extends React.Component {
-  render() {
-
-}
-
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
-
+      <div>
+        Hello World
+      </div>
     );
   }
 }
